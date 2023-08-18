e170c09662401ed99e97663df995fcefd82fadea
markosnop <markos.vpd@gmail.com>
2023-08-17 21:49:57 -0300

    mk

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import Contents from "./Contents";
+
+function Home() {
+    return(
+        <div>
+            <Contents nome={nome} lista={listadeTarefas}
+         </div>
+    )
+}
+export default Home
--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import {createBrowserRouter, RouterProvider}   from 'react-router-dom'
+import Sobre from './Sobre';
+import Contato from './Contato';
+
+const router= createBrowserRouter([
+{
+  path: '/',
+  element: <App/>,
+  children:[
+    {
+      path:'home'
+      
+    },
+    { 
+      path:'sobre',
+      element: <Sobre />
+    },
+    {
+      path: 'Contato',
+      element: <Contato />
+    }
+  ]
+}
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={(router)}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
